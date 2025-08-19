7d414b2affd57f4ab871f5dcddb234d9fb74337f
omarse7a <omarsamehcs@gmail.com>
2025-08-19 15:10:45 +0300

    feat: add address entity

--- FoodApp/Data/ApplicationDbContext.cs
+++ FoodApp/Data/ApplicationDbContext.cs
@@ -6,8 +6,19 @@ namespace FoodApp.Data
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
+            : base(options){}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {           
+            base.OnModelCreating(builder);
+
+            builder.Entity<Address>()
+                .HasOne(a => a.ApplicationUser)
+                .WithMany(u => u.Addresses)   // 1-to-many
+                .HasForeignKey(a => a.ApplicationUserId)
+                .OnDelete(DeleteBehavior.Cascade); // optional: cascade delete addresses if user deleted
         }
+
+        public DbSet<Address> Addresses { get; set; }
     }
 }
